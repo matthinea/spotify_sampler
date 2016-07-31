77b6077e0ff48e948dd8e7425a1d7a3cfe4a6506
“Matthew <matthew.hinea@gmail.com>
2016-07-30 18:34:11 -0700

    add embedded audio preview to results page

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -4,10 +4,19 @@ class SongController < ApplicationController
   end
 
   def song
-    song = RSpotify::Track.search()
+    get_song
   end
 
   def about
     
+  end
+
+  private 
+
+  def get_song
+    @word = Word.all.sample.entry.chomp
+    tracks = RSpotify::Track.search(@word)
+    get_song while tracks.empty? # guarantee results
+    @track = tracks.sample
   end
 end

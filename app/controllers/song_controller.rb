8a1f58d620e642e7338e0373a8d389c43d464d85
“Matthew <matthew.hinea@gmail.com>
2016-09-10 12:02:20 -0700

    refactor view to work with new API calls

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -11,7 +11,7 @@ class SongController < ApplicationController
 
   def song
     @access_token = retrieve_access_token
-    get_song
+    get_song(@access_token)
   end
 
   def about
@@ -23,44 +23,54 @@ class SongController < ApplicationController
 
   private 
 
-  def get_song
-    @word = Word.all.sample
-    entry = @word.entry.chomp
+  def get_song(access_token)
+    word = Word.all.sample
+    entry = word.entry.chomp
 
-    uri = URI.parse("https://api.spotify.com/v1/search?q=#{entry}&type=track")
-    request = Net::HTTP::Get.new(uri)
-    request["Authorization"] = "Bearer BQC1-RiV7op7pWHN7zpaw3Zp-TBB3ujh8a6hL46WZRpLJbSYTUFwBB8QVFkm9bVKv5VnaDuKfEXk95qykdJKPg"
+    response = get_api_response(access_token, entry)
+    
+    tracks = parse_response_for_track_list(response)
 
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-      http.request(request)
+    if tracks.empty? 
+      word.destroy!
+      get_song(access_token)
     end
 
-    tracks = JSON.parse(response.body)["tracks"]["items"]
-
-    while tracks.empty? # guarantee results
-      @word.destroy
-      get_song
-    end
-
-
-    # HERE ---- Refactor for new JSON data
     @track = tracks.sample
-    session[:history] << [@track.name, @track.album.name, @track.artists.each.map{|x| x.name}.to_sentence, Time.now.httpdate]
     @word = entry
+
+    add_track_info_to_session_history(@track)
   end
 
   def retrieve_access_token
     uri = URI.parse("https://accounts.spotify.com/api/token")
     request = Net::HTTP::Post.new(uri)
-    request["Authorization"] = "Basic MDNlOWNhNDdiYjYyNGVkNDg5MTBmNmRiNDZhMzRmMjc6YmE4MGY3MGI5NjU4NGZlZjliZDFiZjBiOGRhNWZiMGQ="
+
+    request["Authorization"] = "Basic #{ENV["encoded_client_id_and_secret"]}"
     request.set_form_data(
       "grant_type" => "client_credentials",
       )
-
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
       http.request(request)
     end
 
-    JSON.parse(response.body)["access_token"]
+    access_token = JSON.parse(response.body)["access_token"]
+  end
+
+  def get_api_response(access_token, search_word)
+    uri = URI.parse("https://api.spotify.com/v1/search?q=#{search_word}&limit=40&type=track")
+    request = Net::HTTP::Get.new(uri)
+    request["Authorization"] = "Bearer #{access_token}"
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+      http.request(request)
+    end
+  end
+
+  def parse_response_for_track_list(response)
+    JSON.parse(response.body)["tracks"]["items"]
+  end
+
+  def add_track_info_to_session_history(track)
+    session[:history] << [track["name"], track["album"]["name"], track["artists"].each.map{|x| x["name"]}.to_sentence, Time.now.httpdate]
   end
 end
